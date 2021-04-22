31802c94d41071c9ffc9fad4cdcb21ecaa3dcacc
Daniel Freitas <danifp19@gmail.com>
2021-04-22 20:14:51 -0300

    alteração reunião

--- peg.rkt
+++ peg.rkt
@@ -5,7 +5,7 @@
 (define-language Peg
    (e natural    ; Terminal
      (/ e e)     ; Choice
-     (• e e) ; Sequence
+     (• e e)     ; Sequence
      (* e)       ; Repetition
      (! e)       ; Not complement
      ε           ; Empty
@@ -20,13 +20,45 @@
 ; Syntax for parsing expression evaluation
 (define-extended-language evalPeg Grammar
   [E (e s)]
-  [s (natural ...)]
-)
+  [s (natural ...)
+     ⊥])
 
-#;(define-judgment-form evalPeg
+
+(define-judgment-form evalPeg
   #:mode (eval I I O)
   #:contract (eval G E s)
-
+  [(eval G (e_1 s) s_1)
+   (side-condition (botton? s_1))
+   --------------------------------
+   (eval G ((/ e_1 e_2) s) s_1)]
   
+  
+  [--------------------------------
+   (eval G (natural_1 (natural_1 natural ...)) (natural ...))]
+  
+  [(side-condition (diff? natural_1 natural_2))
+   --------------------------------
+   (eval G (natural_1 (natural_2 natural ...)) ⊥)]
+  
+  [--------------------------------
+   (eval G (natural_1 ()) ⊥)]
+  
+  )
 
-)
+(define-metafunction evalPeg
+  [(botton? ⊥)       #f]
+  [(botton? s_1)     #t])
+
+(define-metafunction evalPeg
+  [(diff? natural_1 natural_1) #f]
+  [(diff? natural_1 natural_2) #t]) 
+
+
+(judgment-holds (eval () (1 (1 1 1)) s) s)
+(judgment-holds (eval () (1 (2 1 1)) s) s)
+(judgment-holds (eval () (1 ()) s) s)
+
+(judgment-holds (eval () ((/ 1 2) (1 1 1)) s) s)
+(judgment-holds (eval () ((/ 1 2) (2 1 1)) s) s) ;;
+
+ 
