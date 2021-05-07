4e9c3f5eb290f4656643a5e415ebd7e5d6c56543
ReginaSarah <rsaramonferrarih@gmail.com>
2021-05-06 21:05:21 -0300

    Reunião 06/05

--- peg.rkt
+++ peg.rkt
@@ -14,15 +14,32 @@
 
 ; Syntax for a PEG grammar
 (define-extended-language Grammar Peg
-  [G ((x e) ...)] ; A grammar is a set of nonterminal definition
+  [G (x e G) ∅] ; A grammar is a set of nonterminal definition
 )
 
 ; Syntax for parsing expression evaluation
 (define-extended-language evalPeg Grammar
   [E (e s)]
   [s (natural ...)
-     ⊥])
+     ⊥]
+  [R e ⊥])
 
+(define-judgment-form evalPeg
+  #:mode (lookup I I O)
+  #:contract (lookup G x R)
+  
+  [--------------------------------
+   (lookup (x_1 e G) x_1 e)]
+
+  [--------------------------------
+   (lookup ∅ x ⊥)]
+
+  [(lookup G x_2 R)
+   (side-condition (diffs? x_1 x_2))
+   --------------------------------
+   (lookup (x_1 e_1 G) x_2 R)]
+  
+)
 
 (define-judgment-form evalPeg
   #:mode (eval I I O)
@@ -78,27 +95,26 @@
    (eval G ((! e) s) s)]
 
   ;Repetition
-  [(eval G (e s) s_1)
-   (side-condition (not-botton? s_1))
+  [(eval G (e s) ⊥)
    -------------------------------
-   (eval G ((* e) s) ⊥)]
+   (eval G ((* e) s) s)]
 
   [(eval G (e s) s_1)
-   (side-condition (rep s_1))
+   (side-condition (botton? s_1))
+   (eval G ((* e) s_1) s_2)
    -------------------------------
-   (eval G ((* e) s) s_1)]
-
-  #;[(eval G (e s) s_1)
-   (eval G (e s_1) s_2)
-   (side-condition (rep? s_1 s_2))
-   -------------------------------
-   (eval G ((* e) s) s_1)]
+   (eval G ((* e) s) s_2)]
 
   ;Non-Terminal
-  [(eval ((x_1 e_1)) (x_1 s) s_1)
-   (side-condition (not-botton? s_1))
+  [(lookup G x e)     
+   (eval G (e s) s_1)
    --------------------------------
-   (eval ((x_1 e_1)) (e_1 s) s_1)]
+   (eval G (x s) s_1)]
+  
+  [(lookup G x ⊥)
+   --------------------------------
+   (eval G (x s) ⊥)]
+
   
 )
 
@@ -106,10 +122,14 @@
 (define-metafunction evalPeg
   [(equal? x x) #t]
   [(equal? x e) #f])
+
 (define-metafunction evalPeg
   [(diff? natural_1 natural_1) #f]
   [(diff? natural_1 natural_2) #t]) 
 
+(define-metafunction evalPeg
+  [(diffs? x_1 x_1) #f]
+  [(diffs? x_1 x_2) #t])
 
 (define-metafunction evalPeg
   [(botton? ⊥)        #f]
@@ -120,53 +140,46 @@
   [(not-botton? s_1)      #f])
 
 
-(define-metafunction evalPeg
-  [(rep ⊥)   #f]
-  [(rep s s_1 s_2 ...) (rep s_1 s_2 ...)]
-  [(rep _ ...)  #t]
-  )
-
-(define-metafunction evalPeg
-  [(rep? s ⊥)                         #f]
-  [(rep? s s s_1 ...)               (rep? s s_1 ...)]
-  [(rep? s s_1)                       #t])
-
-
-
 ;tests for terminal
 (display "Terminal\n")
-(judgment-holds (eval () (1 (1 1 1)) s) s)
-(judgment-holds (eval () (1 (2 1 1)) s) s)
-(judgment-holds (eval () (1 ()) s) s)
+(judgment-holds (eval ∅ (1 (1 1 1)) s) s)
+(judgment-holds (eval ∅ (1 (2 1 1)) s) s)
+(judgment-holds (eval ∅ (1 ()) s) s)
 
 ;tests for choice
 (display "\nChoice\n")
-(judgment-holds (eval () ((/ 1 2) (1 1 1)) s) s)
-(judgment-holds (eval () ((/ 1 2) (2 1 1)) s) s)
-(judgment-holds (eval () ((/ 1 2) ()) s) s)
+(judgment-holds (eval ∅ ((/ 1 2) (1 1 1)) s) s)
+(judgment-holds (eval ∅ ((/ 1 2) (2 1 1)) s) s)
+(judgment-holds (eval ∅ ((/ 1 2) ()) s) s)
 
 (display "\nSequence\n")
-(judgment-holds (eval () ((• 1 2) (1 2 3)) s) s)
-(judgment-holds (eval () ((• 1 2) (2 2 3)) s) s)
-(judgment-holds (eval () ((• 1 2) (1 1 3)) s) s)
+(judgment-holds (eval ∅ ((• 1 2) (1 2 3)) s) s)
+(judgment-holds (eval ∅ ((• 1 2) (2 2 3)) s) s)
+(judgment-holds (eval ∅ ((• 1 2) (1 1 3)) s) s)
 
 (display "\nNot\n")
-(judgment-holds (eval () ((! 1) (1 2 3)) s) s)
-(judgment-holds (eval () ((! 1) (2 2 3)) s) s)
-(judgment-holds (eval () ((! 1) ()) s) s)
+(judgment-holds (eval ∅ ((! 1) (1 2 3)) s) s)
+(judgment-holds (eval ∅ ((! 1) (2 2 3)) s) s)
+(judgment-holds (eval ∅ ((! 1) ()) s) s)
 
 (display "\nEmpty\n")
-(judgment-holds (eval () (ε (2 2)) s) s)
-(judgment-holds (eval () (ε (1 2 3 4 5 6 7)) s) s)
+(judgment-holds (eval ∅ (ε (2 2)) s) s)
+(judgment-holds (eval ∅ (ε (1 2 3 4 5 6 7)) s) s)
 
 (display "\nRepetition\n")
-(judgment-holds (eval () ((* 2) (4 5 6 7)) s) s)
-(judgment-holds (eval () ((* 2) ()) s) s)
-(judgment-holds (eval () ((* 2) (2 2 2 2 4 5 6 7)) s) s) ;era pra consumir todos os 2, na teoria
+(judgment-holds (eval ∅ ((* 2) (4 5 6 7)) s) s)
+(judgment-holds (eval ∅ ((* 2) ()) s) s)
+(judgment-holds (eval ∅ ((* 2) (2 2 2 2 3 4)) s) s) ;era pra consumir todos os 2, na teoria
+;(judgment-holds (eval ∅ ((* ε) (2 2 2 3)) s) s) ;não tire esse ; a menos que saiba o que esta fazendo
 
 ;Não terminal
 (display "\nNon-Terminal\n")
-(judgment-holds (eval ((A 2)) (A (2 2 3 4 5 6 7)) s) s)
+(judgment-holds (eval (A 2 ∅) (A (2 3 4 5 6 7)) s) s)
+(judgment-holds (eval (B 1 (A 2 ∅)) (A (2 3 4 5 6 7)) s) s) ;cebolas na gramatica
+(judgment-holds (eval (B 1 (A B ∅)) (A (1 2 3 4 5 6 7)) s) s)
+(judgment-holds (eval (B 1 (A B ∅)) (C (1 2 3 4 5 6 7)) s) s)
+(judgment-holds (lookup (B 1 (A 2 ∅)) C R) R)
+(judgment-holds (lookup ∅ C R) R)
 
 ;ε* -> vai entrar em loop
 ;excluir exp loop = exp bem formadas
