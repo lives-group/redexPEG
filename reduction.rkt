32260f8892e4c3b669cf3291a7318a5950031e46
ReginaSarah <rsaramonferrarih@gmail.com>
2021-06-24 11:51:47 -0300

    fluindo

--- reduction.rkt
+++ reduction.rkt
@@ -21,11 +21,22 @@
    (--> (in-hole C (natural_1 (natural_1 natural ...)))
         (in-hole C (natural ...))
         "Terminal")
-
-
+   (--> (in-hole C (natural_1 (natural_2 natural ...)))
+        (in-hole C ⊥)
+        ;(side-condition (not (equal? (term natural_1) (term natural_2))))
+        "Terminal ⊥")
+   (--> (in-hole C (natural_1 ()))
+        (in-hole C ⊥)
+        "Terminal () ⊥")
+   (--> (in-hole C ((/ e_1 e_2) (natural ...)))
+        (in-hole C (e_1 (natural ...)))
+        "Alternancia-1")
+   (--> (in-hole C ((/ e_1 e_2) (natural_1 natural ...)))
+        (in-hole C (e_2 (natural_1 natural ...)))
+        "Alternancia-2")
 
    )
   )
 
-(traces red (term (1 (1 2 3))))
+(traces red (term ((/ 1 2) (1 2 3))))
 
