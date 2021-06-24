dae2c4720723e699d4b6e9f78a5ae2a0d2a16f64
daniel <dfreita4s>
2021-06-24 18:27:56 -0300

    algumas reduçoes

--- reduction.rkt
+++ reduction.rkt
@@ -16,8 +16,8 @@
   (reduction-relation 
    Reduct
    ;#:domain (C s)
-
-   ;Choice relation
+   
+   ;Terminal
    (--> (in-hole C (natural_1 (natural_1 natural ...)))
         (in-hole C (natural ...))
         "Terminal")
@@ -28,15 +28,63 @@
    (--> (in-hole C (natural_1 ()))
         (in-hole C ⊥)
         "Terminal () ⊥")
-   (--> (in-hole C ((/ e_1 e_2) (natural ...)))
-        (in-hole C (e_1 (natural ...)))
-        "Alternancia-1")
-   (--> (in-hole C ((/ e_1 e_2) (natural_1 natural ...)))
-        (in-hole C (e_2 (natural_1 natural ...)))
-        "Alternancia-2")
 
+   ;Empty
+   (--> (in-hole C (ε natural ...))
+        (in-hole C (natural ...))
+        "Empty")
+
+   ;Choice
+   (--> (in-hole C ((/ e_1 e_2) (e_1 natural ...)))
+        (in-hole C ((/ e_1 e_2) (natural ...)))
+        "Alternancia-1")
+
+   (--> (in-hole C ((/ e_1 e_2) (e_2 natural ...)))
+        (in-hole C ((/ e_1 e_2) (natural ...)))
+        "Alternancia-2")
+   
+   (--> (in-hole C ((/ e_1 e_2) (e_3 natural ...))) ;caso nao haja mais matches mas ta fazendo coisa que nao precisa 
+        (in-hole C (e_3 natural ...))
+        "Alternancia-3") 
+   
+   ;Sequence
+
+   (--> (in-hole C ((• e_1 e_2) (e_1 e_2 natural ...)))
+        (in-hole C ((• e_1 e_2) (e_2 natural ...)))
+        "Sequencia-1")
+
+   (--> (in-hole C ((• e_1 e_2) (e_1 e_2 natural ...)))
+        (in-hole C ((• e_1 e_2) (natural ...)))
+        "Sequencia-2")
+
+   (--> (in-hole C ((• e_1 e_2) (e_3 natural ...)))
+        (in-hole C (e_3 natural ...))
+        "Sequencia-3")
+
+   
+
+   ;Repetition
+
+   (--> (in-hole C ((* e_1) (e_1 natural ...)))
+        (in-hole C ((* e_1) (natural ...)))
+        "Repetition-1")
+
+   (--> (in-hole C ((* e_1) (e_2 natural ...)))
+        (in-hole C (e_2 natural ...))
+        "Repetition-2"
+        ;(side-condition (diff? e_1 e_2)) ;como usa side contidion
+        )
    )
   )
+(define-metafunction Reduct
+  [(diff? e_1 e_2) #t]
+  [(diff? e_1 e_1)     #f])
+;Choice
+;(traces red (term ((/ 1 2) (1 2 3))))
+;(traces red (term ((/ 1 2) (3 3 3))))
 
-(traces red (term ((/ 1 2) (1 2 3))))
+;Sequence
+;(traces red (term ((• 1 2) (1 2 3))))
 
+;Repetition
+(traces red (term ((* 1) (1 1 1 1 2))))
