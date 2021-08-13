50a37d0c4e045d1b587907f0cb9699636eec9c5c
ReginaSarah <rsaramonferrarih@gmail.com>
2021-08-12 21:30:35 -0300

    reunião 12/08

--- tests-reduction-judgment.rkt
+++ tests-reduction-judgment.rkt
@@ -13,6 +13,16 @@
 
   )
 
+(define (WF l)
+  (cond [(eq? (list-ref (car l) 3) (list '(• 1 2))) (display "uhuul")]
+        [else (print (list-ref (car l) 3))])
+  )
+;implementar o algoritmo do artigo do ford?
+;usar o judgment wf para obter resultados imediatos
+;receber uma gramatica e expressão para verificar se tá WF.
+;função auxiliar - vamos precisar? 
+;colocar numa lista qualquer as exp que a gnt acha que é bem formada
+;final a lista é igual a 1 -> testar
 ;fazer uma meta funçao para verificar se uma gramatica é WF 
 ;(get-result (apply-reduction-relation* red (term (∅ ⊢ () (• 1 2) ↓ (1 3 3) () ⊥ (0)))))
 
@@ -119,6 +129,23 @@
 (test-equal
  (judgment-holds (eval ∅ ((* 1) ()) s) s) ;judgment retornando '(())
  (get-result (apply-reduction-relation* red (term (∅ ⊢ () (* 1) ↓ () () ⊥ (0)))))
+ )
+
+(test-results)
+
+(display "\nNon-terminal\n")
+
+(test-equal
+ (judgment-holds (eval (A (/ (• 0 (• A 1)) ε)
+                          (B (/ (• 1 (• B 2)) ε)
+                             (C (/ 0 (/ 1 2))
+                                (S (• (! (! A)) (• (* 0) (• B (! C)))) ∅)))) (S (0 1 2)) s) s)
+
+ (get-result (apply-reduction-relation* red (term ((A (/ (• 0 (• A 1)) ε)
+                                          (B (/ (• 1 (• B 2)) ε)
+                                             (C (/ 0 (/ 1 2))
+                                                (S (• (! (! A)) (• (* 0) (• B (! C)))) ∅))))
+                                       ⊢ () S ↓ (0 1 2) () ⊥ (0)))))
  )
 
 (test-results)
