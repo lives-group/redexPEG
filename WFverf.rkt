08bd0352ccc77e88ee4c8ca7cd01a2eb3c96cc9f
dfreita4s <danifp19@gmail.com>
2021-09-02 17:44:27 -0300

    reuniao 2/09

--- WFverf.rkt
+++ WFverf.rkt
@@ -38,18 +38,31 @@
       )
   )
 
-(define (get-exp e)
+#;(define (get-exp e)
 
-  (if (eq? (list-ref (car e) 0) (term ∅))
-      (list-ref (car e) 1)
-      (car e))
+    (if (eq? (list-ref (car e) 0) (term ∅))
+        (list-ref (car e) 1)
+        (car e))
       
-  )
+    )
 
 (define (is-WF grammar e) ;vai vir a expressao por exemplo (G (/ (/ 1 2) 2))
   
-  (define exp (get-exp e))
-  (define id (car exp))
+  (if (list? e)
+      (let ((id (car e)))
+        (cond [(eq? id '/)  (and (is-WF grammar (cadr e)) (is-WF grammar (caddr e)))]
+              [else (display "Deu ruim") #f]
+              )
+
+        )
+      (cond [(number? e) #t]
+            [(eq? e 'ε)  #t]
+            [else (display "Nao Terminal")#f]
+            )
+      )
+#|
+  #;(define exp (get-exp e))
+  #;(define id (car e))
   
   (print exp)
   (display " - ")
@@ -59,25 +72,28 @@
       (cond [(number? id)                             #t]
             [(eq? id (term ε))                        #t]
             [(eq? id (term !))                        (is-WF grammar (list (list-ref exp 1)))]
-            [(eq? id (term /))                        (and (is-WF grammar (list (list (list-ref exp 1)))) (is-WF grammar (list (list (list-ref exp 2)))))] 
+            [(eq? id '/)                        (and (is-WF grammar (list (list (list-ref exp 1)))) (is-WF grammar (list (list (list-ref exp 2)))))] 
             [(eq? id (term •))                        (and (is-WF grammar (list (list (list-ref exp 1))))
                                                            (or (verf-judg   (list-ref exp 1)) (is-WF grammar (list (list (list-ref exp 2))))))] ;usar o judgment ⇀ pra testar se consome algo (judgment-hold ⇀ ∅ (• e_1 e_2)) 
             [(eq? id (term *))                        (and (is-WF grammar (list (list (list-ref exp 1))))
                                                            (verf-judg exp))]
               
-            [else "Deu errado"]
+            [else (display "Deu ruim\n") #f];Non terminal
             )
 
       "null")
   ;(is-WF (judgment-holds (lookup ,(car (car e)) ,exp R) R));ta dando errado
   )
-  
+  |#
+)
 ;FUNÇÃO QUE INICIA TUDO - define a gramática e verifica por ela
 (define (inicio e)
   (define grammar (car (car e)))
   (define exp (list-ref (car e) 1))
+  ;(define exp (get-exp e))
   
   (print grammar)
+  ;(print e)
   (display " - ")
 
   (if (eq? (term ∅) grammar)
@@ -87,6 +103,10 @@
   )
 
 ;testar mais
+(is-WF '∅ '(/ 1 2))
+(is-WF '∅ '(/ (/ (/ 1 2) 1) 2))
+(is-WF '∅ '(/ (/ (/ 1 2) 1) (/ 1 2)))
+#|
 (display "\nTerminal\n")
 (inicio (list '(∅ (1))))
 
@@ -114,3 +134,5 @@
 (inicio (list '((B 1 (A B ∅)) (B))))
 (inicio (list '((B 1 (A B ∅)) (C))))
 (inicio (list '((A (• 1 A) ∅) (A))))
+(inicio (list '((A (• A 1) ∅) (A))))
+|#
