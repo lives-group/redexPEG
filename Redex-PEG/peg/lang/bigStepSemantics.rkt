326e99e4fbba2573a8379f12876ccabbd919ec27
emcardoso <eltonm.cardoso@gmail.com>
2022-02-08 10:38:01 -0300

    Raco

---
+++ Redex-PEG/peg/lang/bigStepSemantics.rkt
@@ -0,0 +1,88 @@
+#lang racket
+
+; This module contains the implementation of a big step semmantics for PEG in Redex. 
+
+
+(require redex)
+(require "./peg.rkt")
+
+(provide (all-defined-out))
+
+; Syntax for a big-step PEG evaluation
+(define-extended-language BigStep Grammar
+  [E (e s)]         ;  An evaluation is comprised of a PEG and a input 
+  [s (natural ...)  ;  An input can be: * A squence of terminal symbols
+     ⊥             ;                   * Bottom, meaning an parser error 
+     ε])            ;                   * Empty string (there is nothing to be consumed !)
+
+
+; Judgment for a big-step  peg evaluation 
+(define-judgment-form BigStep
+  #:mode (eval I I O)
+  #:contract (eval G E s)
+  
+  ;Terminal
+  [-------------------------------- 
+   (eval G (natural_1 (natural_1 natural ...)) (natural ...))]
+  
+  [(side-condition (dismatch? natural_1 natural_2))
+   --------------------------------
+   (eval G (natural_1 (natural_2 natural ...)) ⊥)]
+  
+  [--------------------------------
+   (eval G (natural_1 ()) ⊥)]
+  
+ ; choice 
+  [(eval G (e_1 s) (natural ...))
+   --------------------------------
+   (eval G ((/ e_1 e_2) s) (natural ...))]
+
+  [(eval G (e_1 s) ⊥)
+   (eval G (e_2 s) s_1)
+   -------------------------------
+   (eval G ((/ e_1 e_2) s) s_1)]
+
+ 
+  ;Sequence
+  [(eval G (e_1 s) (natural ...))
+   (eval G (e_2 (natural ...)) s_2)
+   -------------------------------
+   (eval G ((• e_1 e_2) s) s_2)]
+
+  [(eval G (e_1 s) ⊥)
+   ------------------------------
+   (eval G ((• e_1 e_2) s) ⊥)]
+
+  ;Not
+  [(eval G (e s) (natural ...) )
+   -------------------------------
+   (eval G ((! e) s) ⊥)]  
+
+  [(eval G (e s) ⊥)
+   -------------------------------
+   (eval G ((! e) s) s)]
+
+  ;Repetition
+  [(eval G (e s) ⊥)
+   -------------------------------
+   (eval G ((* e) s) s)]
+
+  [(eval G (e s) (natural ...))
+   (eval G ((* e) (natural ...)) s_2)
+   -------------------------------
+   (eval G ((* e) s) s_2)]
+
+  ;Empty
+  [-------------------------------
+   (eval G (ε s) s)]
+
+  ;Non-Terminal
+  [(eval G ((lookupG G x) s) s_1)
+   --------------------------------
+   (eval G (x s) s_1)]
+  )
+
+; Checks if natural_1 and natural_2 are different
+(define-metafunction BigStep
+  [(dismatch? natural_1 natural_1) #f]
+  [(dismatch? natural_1 natural_2) #t]) 
---
+++ Redex-PEG/peg/lang/peg.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+; This module contains the base Parsing Expression Grammar definitions.
+
+(require redex)
+(require rackcheck)
+(provide (all-defined-out))
+
+; Syntax of parsing expressions
+(define-language Peg
+   (e natural    ; Terminal
+     (/ e e)     ; Choice
+     (• e e)     ; Sequence
+     (* e)       ; Repetition
+     (! e)       ; Not complement
+     ε           ; Empty
+     x)          ; Non-Temrinal 
+    (x variable-not-otherwise-mentioned))
+
+; Syntax for a Parsing Expression Grammar
+(define-extended-language Grammar Peg
+  [G (x e G) ∅] ; A grammar is a set of nonterminal definitions
+  )
+
+
+
+; Look-up for a nontermila on a grammar
+  
+(define-metafunction Grammar
+  [(lookupG (x e G) x) e]
+  [(lookupG (x_1 e G) x_2) (lookupG G x_2)]
+  [(lookupG ∅ x) ,(error 'lookupG "not found: ~e" (term x))]
+  )
+
+
