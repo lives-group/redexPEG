753f5a4afca62017eaedb120d9f523341a8d2f38
emcardoso <eltonm.cardoso@gmail.com>
2022-04-05 00:13:32 -0300

    Typed Peg Expression in Racket

--- TypedPEG/TypePEG.rkt
+++ TypedPEG/TypePEG.rkt
@@ -5,7 +5,7 @@
    [E (• E E)
       (/ E E)
       (★ E)
-      (¬ E)
+      (! E)
       x
       string
       ϵ]
@@ -44,29 +44,40 @@
 
   [(types Γ E (_ hs))
     ---------------------------
-    (types Γ (¬ E) (#t  hs) ) ]
+    (types Γ (! E) (#t  hs) ) ]
 
   [(types Γ E (#f hs))
     ---------------------------
     (types Γ (★ E) (#t  hs) ) ]
   
-  [(types Γ x (boolean hs) )
+  [
    (side-condition (∉ x hs))
     ---------------------------
-    (types Γ x (boolean hs)  ) ]
+    (types (x : (boolean hs) Γ) x (boolean hs)  ) ]
 
+   [(types Γ x (boolean hs))
+     (side-condition (∉ x hs))
+     (side-condition (≠ x_1 x))
+    ---------------------------
+    (types (x_1 : _ Γ) x (boolean hs)  ) ]
   
   )
 
 
 (define-metafunction TypedPegExp
   ⊗ : τ τ -> τ
-  [(⊗ ( boolean (x ...)) ( boolean_1 (x_1 ...))) ( (∧ boolean  boolean_1) (∪ (x ...) (⇒ boolean (x_1 ...)))) ]
+  [(⊗ ( boolean hs) ( boolean_1 hs_1)) ( (∧ boolean  boolean_1) (∪ hs (⇒ boolean hs_1))) ]
   )
 
 (define-metafunction TypedPegExp
   ⊕ : τ τ -> τ
-  [(⊕ ( boolean (x ...)) ( boolean_1 (x_1 ...))) ( (∨ boolean  boolean_1) (∪ (x ...) (x_1 ...)) ) ]
+  [(⊕ ( boolean hs) ( boolean_1 hs_1)) ( (∨ boolean  boolean_1) (∪ hs hs_1) ) ]
+  )
+
+(define-metafunction TypedPegExp
+  ≠ : any any -> boolean
+  [(≠ x x) #f]
+  [(≠ x x_1) #t]
   )
 
 (define-metafunction TypedPegExp
@@ -84,11 +95,13 @@
 (define-metafunction TypedPegExp
   ∧ : boolean boolean -> boolean
   [(∧ #t #t) #t]
-  [(∧ _ _) #t]
+  [(∧ _ _) #f]
   )
 
 (define-metafunction TypedPegExp
   ∪ : hs hs -> hs
+  [(∪ ∅ ∅ ) ∅]
+  [(∪ () () ) ∅]
   [(∪ ∅ (x_2 ...) ) (x_2 ...)]
   [(∪ () (x_2 ...) ) (x_2 ...)]
   [(∪ (x_1 ...) ∅)  (x_1 ...)]
