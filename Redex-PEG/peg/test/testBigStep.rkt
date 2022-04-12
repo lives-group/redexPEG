794caf67f35db3a1ba2557bdd60902e10cce81c2
Regina Sarah <monferrari.regina@estudante.ufjf.br>
2022-04-11 22:26:09 -0300

    socorro

--- Redex-PEG/peg/test/testBigStep.rkt
+++ Redex-PEG/peg/test/testBigStep.rkt
@@ -1,65 +1,47 @@
 #lang racket
 
 (require redex)
+(require rackunit)
+(require rackcheck)
+(require peg-gen)
 (require "../lang/peg.rkt")
 (require "../lang/bigStepSemantics.rkt")
+(require "../lang/smallStepSemantics.rkt")
 
-;Tests
-
-
-;Tests for Terminal
-
-(display "Terminal\n")
-(test-equal (judgment-holds (eval ∅ (1 (1 1 1)) s) s) (list (term (1 1))))
-(test-equal (judgment-holds (eval ∅ (1 (2 1 1)) s) s) (list (term ⊥)))
-(test-equal (judgment-holds (eval ∅ (1 ()) s) s)      (list (term ⊥)))
-(test-results)
-
-;Tests for Choice
-(display "\nChoice\n")
-
-(test-equal (judgment-holds (eval ∅ ((/ 1 2) (1 1 1)) s) s)      (list (term (1 1))))
-(test-equal (judgment-holds (eval ∅ ((/ 1 2) (2 1 1)) s) s)      (list (term (1 1))))
-(test-equal (judgment-holds (eval ∅ ((/ 1 2) ()) s) s)           (list (term ⊥)))
-(test-equal (judgment-holds (eval ∅ ((/ 1 2) (3 4)) s) s)        (list (term ⊥)))
-(test-results)
-
-;Tests for Sequence
-(display "\nSequence\n")
-
-(test-equal (judgment-holds (eval ∅ ((• 1 2) (1 2 3)) s) s) (list (term (3))))
-(test-equal (judgment-holds (eval ∅ ((• 1 2) (2 2 3)) s) s) (list (term ⊥)))
-(test-equal (judgment-holds (eval ∅ ((• 1 2) (1 1 3)) s) s) (list (term ⊥)))
-(test-results)
-
-;Tests for Not
-(display "\nNot\n")
-
-(test-equal (judgment-holds (eval ∅ ((! 1) (1 2 3)) s) s) (list (term ⊥)))
-(test-equal (judgment-holds (eval ∅ ((! 1) (2 2 3)) s) s) (list (term (2 2 3))))
-(test-equal (judgment-holds (eval ∅ ((! 1) ()) s) s)  (list empty))
-(test-results)
-
-;Tests for Empty
-(display "\nEmpty\n")
-
-(test-equal (judgment-holds (eval ∅ (ε (2 2)) s) s) (list (term (2 2))))
-(test-equal (judgment-holds (eval ∅ (ε (1 2 3 4 5 6 7)) s) s) (list (term (1 2 3 4 5 6 7))))
-(test-results)
-
-;Tests for Repetition
-(display "\nRepetition\n")
-
-(test-equal (judgment-holds (eval ∅ ((* 2) (4 5 6 7)) s) s)  (list (term (4 5 6 7))))
-(test-equal (judgment-holds (eval ∅ ((* 2) ()) s) s)  (list (term ())))
-(test-equal (judgment-holds (eval ∅ ((* 2) (2 2 2 2 3 4)) s) s) (list (term (3 4))))
-(test-results)
-
-;Tests for Non-Terminal
-(display "\nNon-Terminal\n")
-
-(test-equal (judgment-holds (eval (A 2 ∅) (A (2 3 4 5 6 7)) s) s) (list (term (3 4 5 6 7))))
-(test-equal (judgment-holds (eval (B 1 (A 2 ∅)) (A (2 3 4 5 6 7)) s) s) (list (term (3 4 5 6 7))))
 (test-equal (judgment-holds (eval (B 1 (A B ∅)) (A (1 2 3 4 5 6 7)) s) s) (list (term (2 3 4 5 6 7))))
-;(test-equal (judgment-holds (eval (B 1 (A B ∅)) (C (1 2 3 4 5 6 7)) s) s) (list (term ⊥)))
 (test-results)
+
+
+;; Testing with redex-check
+
+#;(redex-check Peg ;; Name of a language 
+               ;(G ⊢ (C ...) e ↓ (natural ...) () D (0)) ;; Pattern
+               ;; Racket expression that returns a boolean 
+               )
+
+
+(define (get-result l)
+
+  (if (eq? (list-ref (car l) 7) 'suc)
+      (list (list-ref (car l) 5))
+      (list '⊥))
+
+  )
+
+(define (getGrammar e)
+  (car e)
+  )
+
+(define (getExpression e)
+  (car (cdr e))
+  )
+
+(define-property test-judgm-reduct ([peg  (gen:peg 3 5 2)])
+  ;(display (get-result (apply-reduction-relation* red (term (,(getGrammar peg) ⊢ () ,(getExpression peg) ↓ (1 2 3) () ⊥ (0))))))
+  (check-equal?  (judgment-holds (eval ,(getGrammar peg) ,((getExpression peg) (1 2 3)) s) s)  
+                 (get-result (apply-reduction-relation* red (term (,(getGrammar peg) ⊢ () ,(getExpression peg) ↓ (1 2 3) () ⊥ (0))))))
+  )
+(check-property (make-config #:tests 20) test-judgm-reduct)
+
+
+   
