326d5de7a2387e75e73b50ead64b5de0dd689a6d
ReginaSarah <monferrari.regina@estudante.ufjf.br>
2023-08-07 16:18:21 -0300

    Agora vai. Teste com tipo

--- TypedPEG/TestsTypeInference-refactor-2.rkt
+++ TypedPEG/TestsTypeInference-refactor-2.rkt
@@ -22,6 +22,9 @@
 (define (get-type l)
   (caddr l))
 
+(define (get-type-of-typed-peg peg)
+  (solution2context (infer (peg2struct peg)))
+  )
 
 (define (import-gen-grm l)
   (match l
@@ -54,74 +57,86 @@
 
 (define-property type-checks([peg  (gen:peg 3 5 2)])
   (println peg)
-  (check-equal?  (testgen peg) (findTrue (teste peg)) )
+  (check-types  (get-type-of-typed-peg peg)
+                 (car (car (teste peg)))
+                 )
+  )
+
+;verifica se o tamanho das listas são iguais, se não forem, já retorna falso
+;se for, ele compara as listas 
+(define (check-types type_1 type_2)
+  (cond [(not (equal? (length type_1) (length type_2))) #f]
+        [else (equal? (list->set type_1) (list->set type_2))]
+        )
   )
 
 (define-property simple-check ([peg  (gen:peg 3 2 2)])
-  ;(println peg)
-  (findTrue (teste peg)) 
+  (println peg)
+  (findTrue (teste peg))
+  
   )
 
 
-;(check-property (make-config #:tests 15) type-checks)
-(check-property (make-config #:tests 10
+
+(check-property (make-config #:tests 1) type-checks)
+#;(check-property (make-config #:tests 1
                              #:deadline (+ (current-inexact-milliseconds) (* 60 3000))
                              ) simple-check
-                )
+                               )
 
 ;(teste (last (sample (gen:peg 2 2 3) 1)))
 
 ;((C ε (B C (A 1 ∅))) A ((A #f ()) (B #t (C)) (C #t ())))
 #;(apply-reduction-relation*
- constraint-solve
- (term (((A (v 3)) (B (v 2)) (C (v 1)))
-        ()
-        (∧
-         (∧
+   constraint-solve
+   (term (((A (v 3)) (B (v 2)) (C (v 1)))
+          ()
           (∧
-           ((v 1) ≡ (#t ()))
            (∧
-            (C ≡ (v 3))
-            ((v 2) ≡ (clone (v 3) C))))
-          ((v 3) ≡ (#f ())))
-         (∧
-          (A ≡ (v 1))
-          ((v 0) ≡ (clone (v 1) A)))))))
+            (∧
+             ((v 1) ≡ (#t ()))
+             (∧
+              (C ≡ (v 3))
+              ((v 2) ≡ (clone (v 3) C))))
+            ((v 3) ≡ (#f ())))
+           (∧
+            (A ≡ (v 1))
+            ((v 0) ≡ (clone (v 1) A)))))))
 
 
 #;(apply-reduction-relation* constraint-solve (term (((C (v 1)) (E (v 2)) (S (v 5)))
-  ()
-  (∧
-   (∧
-    (∧
-     (∧
-      (∧
-       ((v 3) ≡ (#t ()))
-       ((v 4) ≡ (#t ())))
-      ((v 1) ≡ (× (v 3) (v 4))))
-     (∧
-      (∧
-       ((v 3) ≡ (#f ()))
-       ((v 4) ≡ (#f ())))
-      ((v 2) ≡ (+ (v 3) (v 4)))))
-    (∧
-     (∧
-      (∧
-       (E ≡ (v 8))
-       ((v 6) ≡ (clone (v 8) E)))
-      ((v 7) ≡ (#t ())))
-     ((v 5) ≡ (× (v 6) (v 7)))))
-   (∧
-    (∧
-     (∧
-      (C ≡ (v 11))
-      ((v 9) ≡ (clone (v 11) C)))
-     (∧
-      (E ≡ (v 12))
-      ((v 10)
-       ≡
-       (clone (v 12) E))))
-    ((v 0) ≡ (× (v 9) (v 10))))))))
+                                                     ()
+                                                     (∧
+                                                      (∧
+                                                       (∧
+                                                        (∧
+                                                         (∧
+                                                          ((v 3) ≡ (#t ()))
+                                                          ((v 4) ≡ (#t ())))
+                                                         ((v 1) ≡ (× (v 3) (v 4))))
+                                                        (∧
+                                                         (∧
+                                                          ((v 3) ≡ (#f ()))
+                                                          ((v 4) ≡ (#f ())))
+                                                         ((v 2) ≡ (+ (v 3) (v 4)))))
+                                                       (∧
+                                                        (∧
+                                                         (∧
+                                                          (E ≡ (v 8))
+                                                          ((v 6) ≡ (clone (v 8) E)))
+                                                         ((v 7) ≡ (#t ())))
+                                                        ((v 5) ≡ (× (v 6) (v 7)))))
+                                                      (∧
+                                                       (∧
+                                                        (∧
+                                                         (C ≡ (v 11))
+                                                         ((v 9) ≡ (clone (v 11) C)))
+                                                        (∧
+                                                         (E ≡ (v 12))
+                                                         ((v 10)
+                                                          ≡
+                                                          (clone (v 12) E))))
+                                                       ((v 0) ≡ (× (v 9) (v 10))))))))
 
 
 ;peg = ((Q (• A J) (A (• ε 0) (J (• ε ε) ∅)))
